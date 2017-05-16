51c0c00cb8d658c5f31922ba974ac17672e7d71a
SyntaxAce <pexo.yt.pl@gmail.com>
2017-05-16 22:31:20 +0200

    Still debuging movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour {
     NavMeshAgent myNavMesh;
-    Vector3 mousePos, MovementDestination;
+    Vector3 mousePos, MovementDestination, DEBUG; // DEBUG is a hit.point with y set to 0
     RaycastHit hit;
     Ray TargetRay;
     // Use this for initialization
@@ -15,14 +15,23 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void Update () {
+        mousePos = Input.mousePosition;
+        TargetRay = Camera.main.ScreenPointToRay(mousePos);
+        Physics.Raycast(TargetRay, out hit);
+        DEBUG = hit.point;
+        DEBUG.y = 0;
+        MovementDestination = DEBUG - transform.position; 
+
+        Debug.DrawRay(Camera.main.transform.position, DEBUG, Color.red);
+        //Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red);
 
         if (Input.GetMouseButtonDown(1))
         {
             mousePos = Input.mousePosition;
             TargetRay = Camera.main.ScreenPointToRay(mousePos);
-            Physics.Raycast(TargetRay, out hit);
+            Physics.Raycast(TargetRay, out hit,9);
             MovementDestination = hit.point - transform.position;
-            MovementDestination.y = 0;
+            //MovementDestination.y = 0;
             mousePos = Input.mousePosition;
             myNavMesh.SetDestination(MovementDestination);
             print(myNavMesh.destination);
