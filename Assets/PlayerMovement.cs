fff7c2a6741ed9dc419d59efccf35df9b84960b0
SyntaxAce <pexo.yt.pl@gmail.com>
2017-05-17 07:39:13 +0200

    Fixed RMB movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,37 +5,25 @@ using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour {
     NavMeshAgent myNavMesh;
-    Vector3 mousePos, MovementDestination, DEBUG; // DEBUG is a hit.point with y set to 0
+    Vector3 mousePos, MovementDestination, DEBUG;
     RaycastHit hit;
     Ray TargetRay;
-    // Use this for initialization
     void Start () {
         myNavMesh = GetComponent<NavMeshAgent>();
 
 	}
 
 	void Update () {
-        mousePos = Input.mousePosition;
-        TargetRay = Camera.main.ScreenPointToRay(mousePos);
-        Physics.Raycast(TargetRay, out hit);
-        DEBUG = hit.point;
-        DEBUG.y = 0;
-        MovementDestination = DEBUG - transform.position; 
-
-        Debug.DrawRay(Camera.main.transform.position, DEBUG, Color.red);
-        //Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red);
-
         if (Input.GetMouseButtonDown(1))
         {
             mousePos = Input.mousePosition;
             TargetRay = Camera.main.ScreenPointToRay(mousePos);
-            Physics.Raycast(TargetRay, out hit,9);
-            MovementDestination = hit.point - transform.position;
+            Physics.Raycast(TargetRay, out hit);
+            MovementDestination = hit.point;
             //MovementDestination.y = 0;
             mousePos = Input.mousePosition;
             myNavMesh.SetDestination(MovementDestination);
             print(myNavMesh.destination);
-            print("set");
         }
 	}
 }
